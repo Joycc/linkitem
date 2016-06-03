3e9e7c86c6b37f8286a7b7bbfd45482d6d9aace5
Joycc <liujue0709@gmail.com>
2016-06-03 15:20:46 +0800

    fix some unknown bug

--- doudou.js
+++ doudou.js
@@ -11,20 +11,20 @@
 
 
 (function( window, undefined ) {
-
+	
 	var Doudou=function(name){
-
+		
     /**
      * Attribute
-     *
-     *
+     * 
+     * 
      */
 		this.MAX_ROWS=14;
 		this.MAX_COLS=14;
 		this.MIN_ROWS=6;
 		this.MIN_COLS=6;
 		this.MAX_TYPES=30;
-
+		
 		this.lineWidth=3;
 		this.Pictypes=15;
 		this.picWidth=44;
@@ -39,7 +39,7 @@
 				433,476,519,562,605,648,691,734,777,820,
 				863,907,950,993,1036,1080,1123,1169,1209,1252];
 		 * Modify here from color to small picture.
-     	*
+     	* 
 		this.colortypes=['#ffffff','#330055','#0000aa','#7700ff',
 					 '#005500','#335555','#0055aa','#7733ff',
 					 '#00aa00','#33aa55','#00aaaa','#7799ff',
@@ -53,69 +53,69 @@
 	        row: 0,
 	        col: 0
 	    };
-
+		
 		this.lastSelectPoint = {
 			row:0,
 			col:0
 		};
-
+		
 		this.rows=0;
-		this.cols=0;
+		this.cols=0;		
 		this.fullMap=new Array();
-
+		
 		this.mapCanvasID="myCanvas";
 		this.mapContext;
 		this.mapCanvas;
-
+		
 		this.controlCanvasID="conCanvas";
 		this.controlContext;
 		this.controlCanvas;
-
+		
 		this.lineCanvasID="lineCanvas";
 		this.lineContext;
 		this.lineCanvas;
-
+		
 		this.success=function(){};
 		this.passthrough = function(){};
 		this.clearOne = function(){};
 		/*
-		this.SoundCG = new Audio('chuangguan.wav');
-		this.SoundSelect = new Audio('click.wav');
-		this.SoundDisappear = new Audio('dispear.wav');
-		this.SoundResort = new Audio('resort.wav');
-		this.SoundNoDisappear = new Audio('nodispear.wav');
+		this.SoundCG = new Audio('chuangguan.wav');  
+		this.SoundSelect = new Audio('click.wav');  
+		this.SoundDisappear = new Audio('dispear.wav');  
+		this.SoundResort = new Audio('resort.wav');  
+		this.SoundNoDisappear = new Audio('nodispear.wav');  
 		*/
 		this.SoundCG;
 		this.SoundSelect;
 		this.SoundDisappear;
 		this.SoundResort;
-		this.SoundNoDisappear;
-
-		this.IsNeedDrawLine = true;
-
+		this.SoundNoDisappear;		
+		 
+		this.IsNeedDrawLine = true;		
+		
 		this.icount=0;
 		this.score=0;
-		this.browser={
-			versions:function(){
-				var u = navigator.userAgent, app = navigator.appVersion;
-				return {
-					trident: u.indexOf('Trident') > -1, //IE core
-					presto: u.indexOf('Presto') > -1, //opera core
-					webKit: u.indexOf('AppleWebKit') > -1, //apple¡¢google core
-					gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //firefox core
+		this.browser={    
+			versions:function(){            
+				var u = navigator.userAgent, app = navigator.appVersion;            
+				return {                
+					trident: u.indexOf('Trident') > -1, //IE core                
+					presto: u.indexOf('Presto') > -1, //opera core                
+					webKit: u.indexOf('AppleWebKit') > -1, //apple¡¢google core                
+					gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //firefox core                
 					mobile: !(u.indexOf('Windows')>-1) &&
-						(!!u.match(/AppleWebKit.*Mobile.*/)||!!u.match(/AppleWebKit/)), //mobile
-					ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios
-					android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android
-					iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //iPhone QQHD
-					iPad: u.indexOf('iPad') > -1, //iPad
-					webApp: u.indexOf('Safari') == -1 //web
+						(!!u.match(/AppleWebKit.*Mobile.*/)||!!u.match(/AppleWebKit/)), //mobile                
+					ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios                
+					android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android                
+					iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //iPhone QQHD                
+					iPad: u.indexOf('iPad') > -1, //iPad                
+					webApp: u.indexOf('Safari') == -1 //web           
 				};
 			}()
-		};
-		this.checkrows=this.browser.versions.mobile ?
+		}; 
+		this.checkrows=this.browser.versions.mobile ? 
 						parseInt((window.innerHeight+2)/this.picHeight)-2 :10;
-		this.checkcols=this.browser.versions.mobile ?
+		this.checkcols=this.browser.versions.mobile ? 
 						parseInt((window.innerWidth+2)/this.picWidth)-2 : 14;
 	};
 	Doudou.prototype={
@@ -124,7 +124,7 @@
 	     * Click a picture on the map
 	     * @param {row} row number
 	     * @param {col} col number
-	     */
+	     */		
 		select:function(row,col){
 			if( row>0 && row<=this.rows && col>0 && col<=this.cols){
 				if (this.fullMap[row][col]==0) {
@@ -132,8 +132,8 @@
 				}
 				if(this.selectedPoint.row==0 || this.selectedPoint.col==0){
 					this.selectedPoint.row=row;
-					this.selectedPoint.col=col;
-					this.drawBorder(this.controlContext,row,col,'#00ffaa');
+					this.selectedPoint.col=col;	
+					this.drawBorder(this.controlContext,row,col,'#00ffaa');	
 					//this.SoundSelect.currentTime=0;
 					this.SoundSelect.play();
 				}
@@ -141,48 +141,48 @@
 					if (this.selectedPoint.row==row && this.selectedPoint.col==col){
 						return;
 					}
-					if (this.fullMap[this.selectedPoint.row][this.selectedPoint.col]==this.fullMap[row][col]){
+					if (this.fullMap[this.selectedPoint.row][this.selectedPoint.col]==this.fullMap[row][col]){						
 						if (this.checkRoad(this.selectedPoint.row,this.selectedPoint.col,row,col)){
 							this.clearBorder(this.controlContext,this.selectedPoint.row,this.selectedPoint.col);
 							this.fullMap[this.selectedPoint.row][this.selectedPoint.col]=0;
 							this.fullMap[row][col]=0;
-							this.SoundDisappear.play();
+							this.SoundDisappear.play();  
 							this.clearOne();
 							this.moveMap(row,col,this.selectedPoint.row,this.selectedPoint.col);
 							this.drawMap();
-							this.checkGameOver();
+							this.checkGameOver();								
 							this.selectedPoint.row=0;
-							this.selectedPoint.col=0;
+							this.selectedPoint.col=0;	
 							this.lastSelectPoint.row=0;
-							this.lastSelectPoint.col=0;
+							this.lastSelectPoint.col=0;		
 
 						}
 						else{
 							if (this.lastSelectPoint.row==row && this.lastSelectPoint.col==col){
 								this.clearBorder(this.controlContext,this.selectedPoint.row,this.selectedPoint.col);
 								this.selectedPoint.row=row;
-								this.selectedPoint.col=col;
-								this.drawBorder(this.controlContext,row,col,'#00ffaa');
+								this.selectedPoint.col=col;			
+								this.drawBorder(this.controlContext,row,col,'#00ffaa');	
 								this.lastSelectPoint.row=0;
 								this.lastSelectPoint.col=0;
 							}
 							else{
 								this.lastSelectPoint.row=row;
 								this.lastSelectPoint.col=col;
-								this.SoundNoDisappear.play();
+								this.SoundNoDisappear.play();								
 							}
 						}
 					}
 					else{
 						this.clearBorder(this.controlContext,this.selectedPoint.row,this.selectedPoint.col);
 						this.selectedPoint.row=row;
-						this.selectedPoint.col=col;
+						this.selectedPoint.col=col;			
 						this.drawBorder(this.controlContext,row,col,'#00ffaa');
-						this.SoundSelect.play();
-					}
+						this.SoundSelect.play();							
+					}								
 				}
 			}
-		},
+		},	
 		checkGameOver : function() {
 			this.totalCount--;
 			this.totalCount--;
@@ -191,7 +191,7 @@
 				if(this.moveType<8){
 					//alert("Congradulations!,Go Next!");
 					this.moveType++;
-					this.passthrough();
+					this.passthrough();					
 			    	this.createmap(this.rows,this.cols);
 			    	//this.initCanvas();
 			    	this.drawMap();
@@ -208,9 +208,9 @@
 				 	this.icount=this.icount+60;
 				}
 				this.IsNeedDrawLine=true;
-			}
+			}			
 		},
-
+		
 		manResort : function(){
 			this.IsNeedDrawLine=false;
 			this.resort();//
@@ -219,22 +219,22 @@
 			 	this.icount=this.icount+60;
 			}
 			this.IsNeedDrawLine=true;
-
+			
 		},
-
-
+		
+		
 		isAlive : function(){
 			for (var i=1;i<=this.rows;i++){
 				for(var j=1;j<=this.cols;j++){
 					if (this.fullMap[i][j]>0){
 						if (this.isAliveOther(i,j)) return true;
 					}
-				}
+				}	
 			}
 			return false;
 		},
-
-
+		
+		
 		isAliveOther : function (_row,_col){
 			for (var i = _row ; i<=this.rows ;i++){
 				for(var j=1;j<=this.cols;j++){
@@ -246,20 +246,20 @@
 							}
 						}
 					}
-				}
+				}	
 			}
 			return false;
 		},
-
-
-		moveMap : function (row1,col1,row2,col2){
+		
+		
+		moveMap : function (row1,col1,row2,col2){			
 			/**
 			* moveType==1 move down
-			*/
-		    switch (this.moveType){
+			*/		
+		    switch (this.moveType){	
 			     /**
 			     * moveType==1 move down
-			     */
+			     */		
 		    	case 1:
 					if (col1==col2 && row1>row2){col2=row1;row1=row2;row2=col2;col2=col1;}
 					for(i=row1;i>0;i--){this.fullMap[i][col1]=this.fullMap[i-1][col1];}
@@ -267,8 +267,8 @@
 					break;
 			     /**
 			     * moveType==2 move up
-			     */
-		    	case 2:
+			     */		
+		    	case 2:	    	
 					if (col1==col2 && row1<row2){
 						col2=row1;row1=row2;row2=col2;col2=col1;
 					}
@@ -281,8 +281,8 @@
 					break;
 			     /**
 			     * moveType==3 move left
-			     */
-		    	case 3:
+			     */		
+		    	case 3:	    	
 					if (row1==row2 && col1<col2){
 						row2=col1;col1=col2;col2=row2;row2=row1;
 					}
@@ -295,8 +295,8 @@
 					break;
 			     /**
 			     * moveType==1 move right
-			     */
-		    	case 4:
+			     */		
+		    	case 4:	
 					if (row1==row2 && col1>col2){
 						row2=col1;col1=col2;col2=row2;row2=row1;
 					}
@@ -307,11 +307,11 @@
 						this.fullMap[row2][i]=this.fullMap[row2][i-1];
 					}
 					break;
-
+						
 			     /**
 			     * moveType==5 split up down
-			     */
-		    	case 5:
+			     */		
+		    	case 5:	    	
 		    		var _minrow=parseInt(this.rows/2);
 		    		var _maxrow=_minrow+1;
 		    		if (col1==col2){
@@ -319,9 +319,9 @@
 							col2=row1;row1=row2;row2=col2;col2=col1;
 		    			}
 		    			if(row1>row2 && row2>=_maxrow){
-							col2=row1;row1=row2;row2=col2;col2=col1;
+							col2=row1;row1=row2;row2=col2;col2=col1;		    				
 		    			}
-		    		}
+		    		} 
 					if (row1<=_minrow){
 						for(i=row1;i<=_minrow;i++){
 							this.fullMap[i][col1]= (i==_minrow ? 0 : this.fullMap[i+1][col1]);
@@ -335,17 +335,17 @@
 						for(i=row2;i<=_minrow;i++){
 							this.fullMap[i][col2]=(i == _minrow ? 0 : this.fullMap[i+1][col2]);
 						}
-					}else{
+					}else{						
 						for(i=row2;i>=_maxrow;i--){
 							this.fullMap[i][col2]=(i==_maxrow?0:this.fullMap[i-1][col2]);
 						}
 					}
-					break;
-
+					break;		    	
+					
 			     /**
 			     * moveType==6 gather up down
-			     */
-				case 6:
+			     */		
+				case 6:	    	
 		    		var _minrow=parseInt(this.rows/2);
 		    		var _maxrow=_minrow+1;
 		    		if (col1==col2){
@@ -353,9 +353,9 @@
 							col2=row1;row1=row2;row2=col2;col2=col1;
 		    			}
 		    			if(row1<row2 && row1>=_maxrow){
-							col2=row1;row1=row2;row2=col2;col2=col1;
+							col2=row1;row1=row2;row2=col2;col2=col1;		    				
 		    			}
-		    		}
+		    		} 
 					if (row1<=_minrow){
 						for(i=row1;i>0;i--){
 							this.fullMap[i][col1]=this.fullMap[i-1][col1];
@@ -375,12 +375,12 @@
 						}
 					}
 					break;
-
-
+					
+					
 			     /**
 			     * moveType==7 split left right
-			     */
-		    	case 7:
+			     */		
+		    	case 7:	    	
 		    		var _mincol=parseInt(this.cols/2);
 		    		var _maxcol=_mincol+1;
 		    		if (row1==row2){
@@ -388,9 +388,9 @@
 							row2=col1;col1=col2;col2=row2;row2=row1;
 		    			}
 		    			if(col1>col2 && col2>=_maxcol){
-							row2=col1;col1=col2;col2=row2;row2=row1;
+							row2=col1;col1=col2;col2=row2;row2=row1;		    				
 		    			}
-		    		}
+		    		} 
 					if (col1<=_mincol){
 						for(i=col1;i<=_mincol;i++){
 							this.fullMap[row1][i]= (i==_mincol ? 0 : this.fullMap[row1][i+1]);
@@ -409,12 +409,12 @@
 							this.fullMap[row2][i]=(i==_maxcol?0:this.fullMap[row2][i-1]);
 						}
 					}
-					break;
-
+					break;		    	
+					
 			     /**
 			     * moveType==8 gather left right
-			     */
-				case 8:
+			     */		
+				case 8:	    	
 		    		var _mincol=parseInt(this.cols/2);
 		    		var _maxcol=_mincol+1;
 		    		if (row1==row2){
@@ -422,10 +422,10 @@
 							row2=col1;col1=col2;col2=row2;row2=row1;
 		    			}
 		    			if(col1<col2 && col1>=_maxcol){
-							row2=col1;col1=col2;col2=row2;row2=row1;
+							row2=col1;col1=col2;col2=row2;row2=row1;		    				
 		    			}
-		    		}
-
+		    		} 
+					
 					if (col1<=_mincol){
 						for(i=col1;i>0;i--){
 							this.fullMap[row1][i]=this.fullMap[row1][i-1];
@@ -447,10 +447,10 @@
 					break;
 		    	default:
 		    }
-
+			
 		},
-
-
+		
+		
 		checkRoadRow : function (_col1,_col2,_row){
 			var _min=_col1<_col2?_col1:_col2;
 			var _max=_col1<_col2?_col2:_col1;
@@ -471,10 +471,10 @@
 			}
 			return 0;
 		},
-
+		
 		checkRoad : function(row1,col1,row2,col2){
 			var icheck=0;
-
+			
 			if (Math.abs(row1-row2)+Math.abs(col1-col2)==1){
 				this.drawLine(this.lineContext,row1,col1,row2,col2);
 				return 1;
@@ -498,11 +498,11 @@
 					return 0;
 				}
 			}
-
+			
 			for (i=Math.max(row1,row2);i>=0;i--){
 				icheck=0;
-				if (col1==col2) {break}
-				if ((this.fullMap[i][col1]==0 || i==row1) && (i==row2 || this.fullMap[i][col2]==0)){
+				if (col1==col2) {break}  
+				if ((this.fullMap[i][col1]==0 || i==row1) && (i==row2 || this.fullMap[i][col2]==0)){ 
 					icheck=icheck+ this.checkRoadCol(row1,i,col1);
 					icheck=icheck+ this.checkRoadRow(col1,col2,i);
 					icheck=icheck+ this.checkRoadCol(i,row2,col2);
@@ -510,18 +510,18 @@
 				else{
 					icheck=1;
 				}
-				if (!icheck) {
+				if (!icheck) {					
 					this.drawLine(this.lineContext,row1,col1,i,col1);
 					this.drawLine(this.lineContext,i,col1,i,col2);
 					this.drawLine(this.lineContext,i,col2,row2,col2);
 					return 1;
 				}
-			}
-
+			}			
+			
 			for (i=Math.max(row1,row2)+1;i<this.rows+2;i++){
 				icheck=0;
-				if (col1==col2) {break}
-				if ((this.fullMap[i][col1]==0 || i==row1) && (i==row2 || this.fullMap[i][col2]==0)){
+				if (col1==col2) {break}  
+				if ((this.fullMap[i][col1]==0 || i==row1) && (i==row2 || this.fullMap[i][col2]==0)){ 
 					icheck=icheck+ this.checkRoadCol(row1,i,col1);
 					icheck=icheck+ this.checkRoadRow(col1,col2,i);
 					icheck=icheck+ this.checkRoadCol(i,row2,col2);
@@ -529,18 +529,18 @@
 				else{
 					icheck=1;
 				}
-				if (!icheck) {
+				if (!icheck) {					
 					this.drawLine(this.lineContext,row1,col1,i,col1);
 					this.drawLine(this.lineContext,i,col1,i,col2);
 					this.drawLine(this.lineContext,i,col2,row2,col2);
 					return 1;
 				}
 			}
-
-
+			
+						
 			for (j=Math.max(col1,col2);j>=0;j--){
 				icheck=0;
-				if (row1==row2) {break}
+				if (row1==row2) {break} 
 				if ((j==col1 || this.fullMap[row1][j]==0) && (j==col2 || this.fullMap[row2][j]==0)){
 					icheck=icheck+ this.checkRoadRow(col1,j,row1);
 					icheck=icheck+ this.checkRoadCol(row1,row2,j);
@@ -556,11 +556,11 @@
 					return 1;
 				}
 			}
-
-
+	
+			
 			for (j=Math.max(col1,col2)+1;j<this.cols+2;j++){
 				icheck=0;
-				if (row1==row2) {break}
+				if (row1==row2) {break} 
 				if ((j==col1 || this.fullMap[row1][j]==0) && (j==col2 || this.fullMap[row2][j]==0)){
 					icheck=icheck+ this.checkRoadRow(col1,j,row1);
 					icheck=icheck+ this.checkRoadCol(row1,row2,j);
@@ -578,16 +578,16 @@
 			}
 			return 0;
 		},
-
-
-
-		initCanvas: function(){
-			var that=this;
+		
+		
+		
+		initCanvas: function(){		
+			var that=this;	
 			this.mapCanvas=document.getElementById(this.mapCanvasID);
 			this.mapContext=this.mapCanvas.getContext('2d');
 			this.lineCanvas=document.getElementById(this.lineCanvasID);
 			this.lineContext=this.lineCanvas.getContext('2d');
-
+			
 			var elem = this.controlCanvas=document.getElementById(this.controlCanvasID);
 			this.controlContext=this.controlCanvas.getContext('2d');
 			elem.addEventListener('click',function(evt){
@@ -600,25 +600,25 @@
 			this.mapCanvas.width=this.controlCanvas.width=this.lineCanvas.width=pWidth;
 			this.mapCanvas.height=this.controlCanvas.height=this.lineCanvas.height=pHeight;
 		},
-
+		
 		clearBorder : function(ctx,y,x) {
 			ctx.clearRect(x*this.picWidth+this.leftPadding-1,y*this.picHeight+this.topPadding-1,this.picWidth+2,this.picHeight+2);
 		},
-
-		drawLine : function(ctx,row1,col1,row2,col2){
-
-			if (!this.IsNeedDrawLine) return;
-
+		
+		drawLine : function(ctx,row1,col1,row2,col2){	
+			
+			if (!this.IsNeedDrawLine) return;		
+			
 			var x1=col1*this.picWidth+this.leftPadding+this.picWidth/2;
 			var x2=col2*this.picWidth+this.leftPadding+this.picWidth/2;
 			var y1=row1*this.picHeight+this.topPadding+this.picHeight/2;
 			var y2=row2*this.picHeight+this.topPadding+this.picHeight/2;
-			ctx.beginPath();
+			ctx.beginPath();  
 	    	ctx.strokeStyle = "#1d953f";
 	    	ctx.lineWidth = this.lineWidth;
-			ctx.moveTo(x1,y1);
-			ctx.lineTo(x2,y2);
-			ctx.stroke();
+			ctx.moveTo(x1,y1);  
+			ctx.lineTo(x2,y2);  
+			ctx.stroke();  
 	    	ctx.closePath();
 		},
 		//
@@ -627,14 +627,14 @@
 		    ctx.rect(x*this.picWidth+this.leftPadding, y*this.picHeight+this.topPadding, this.picWidth, this.picHeight);
 		    ctx.lineWidth = 1;
 		    ctx.globalAlpha=0.4;
-			ctx.fillStyle = color;
+			ctx.fillStyle = color;  
 		    ctx.strokeStyle = color;
 		    ctx.fill();
 		    ctx.stroke();
 		    ctx.closePath();
 		},
-
-
+		
+		
 		drawBox : function(ctx,y,x,color){
 		    ctx.beginPath();
 		    ctx.rect(x*this.picWidth+this.leftPadding, y*this.picHeight+this.topPadding, this.picWidth, this.picHeight);
@@ -648,9 +648,8 @@
 		    	ctx.drawImage(this.img,0,this.imgPosition[color],44,41,x*this.picWidth+this.leftPadding, y*this.picHeight+this.topPadding,this.picWidth,this.picHeight);
 		    }
 		},
-
-		drawMap : function(){
-
+		
+		drawMap : function(){			
 	      for(i=0;i < this.rows+2;i++){
 	        for(j=0;j < this.cols+2;j++){
 	        	if(i>=0 && i<=this.rows+1 && j>=0 && j<=this.cols+1){
@@ -659,9 +658,9 @@
 	        }
 	      }
 		},
-
-		randomsort:function (a, b) {
-        	return Math.random()>.5 ? -1 : 1;
+		
+		randomsort:function (a, b) {  
+        	return Math.random()>.5 ? -1 : 1;  
 		},
 
 		resort:function(){
@@ -689,35 +688,35 @@
 
 
 		createmap: function(_rows,_cols) {
-
+			
 			_rows = (_cols==6 && _rows>10) ? 12 : _rows;
 			_rows = (_cols==8 && _rows>10) ? 12 : _rows;
 			_rows = (_cols==14 ) ? 10 : _rows;
-
-
+			
+						
 			_rows=_rows*_cols%2?_rows-1:_rows;
-
+			
 			this.picTypes=parseInt(_rows*_cols/3.5);
 			this.picTypes=this.picTypes>this.MAX_TYPES?this.MAX_TYPES:this.picTypes;
-
-
+			
+			
 			this.rows=Math.min(_rows,this.MAX_ROWS);
 			this.cols=Math.min(_cols,this.MAX_COLS);
 
-
+						
 			this.leftPadding=(window.innerWidth+2-(this.cols+2)*this.picWidth)/2;
 			this.topPadding=(window.innerHeight+2-(this.rows+2)*this.picHeight)/2;
 			this.leftPadding=this.leftPadding>=0 ? this.leftPadding :0;
 			this.topPadding=this.topPadding>=0 ? this.topPadding :0;
-
+			
 			this.totalCount=this.rows*this.cols;
 			var rnd = [];
 			for(var i=1;i<=(this.rows)*(this.cols)/2;i++)
 			{
 			    var r = parseInt(Math.random()*this.picTypes+1);
 			    rnd[rnd.length]=rnd[rnd.length+1]=r;
-			}
-
+			}		
+			
 			var _array1=new Array();
 	      	for(i=0;i<this.rows+2;i++){
 	        	_array1[i]=new Array();
@@ -727,13 +726,13 @@
 			            _array1[i][j] = rnd[k];
 			            rnd.splice(k,1);
 		        	}else{
-		           		_array1[i][j] = 0;
+		           		_array1[i][j] = 0; 
 		        	}
 	        	}
 	      	}
-			this.fullMap=_array1;
+			this.fullMap=_array1;		
 		},
-
+		
 
 
 
@@ -744,13 +743,13 @@
 			//alert(this.mapCanvas.width);
 			//alert(this.mapCanvas.height);
 			//alert(this.fullMap.toString());
-		}
+		}		
 	};
-
+	
 	/*
-	function isArray(o) {
-  	return Object.prototype.toString.call(o) === '[object Array]';
-	}
+	function isArray(o) {  
+  	return Object.prototype.toString.call(o) === '[object Array]';   
+	}  
 	*/
 	window.Doudou=Doudou;
 })(window);
